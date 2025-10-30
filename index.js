cbe359f00280460c8e47551ac0a127653293530a
piani <stasmaccari63@gmail.com>
2025-10-30 21:13:49 +0200

    push

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors"; 
 import connectDB from "./config/db.js";
 import adminRoutes from "./routes/admin.js";
 import carsRoutes from "./routes/cars.js";
@@ -7,16 +8,25 @@ import carsRoutes from "./routes/cars.js";
 dotenv.config();
 
 const app = express();
+
+
+app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
 app.use("/uploads", express.static("uploads")); 
+
 
 connectDB().then(db => {
   app.locals.db = db;
 
+
   app.use("/api/admin", adminRoutes);
   app.use("/api/cars", carsRoutes);
 
-  app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
   });
 });
